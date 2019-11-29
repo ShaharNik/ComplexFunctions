33a93f58026d949f2563679fa3a7b582a508bd4d
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2019-11-29 09:51:21 +0200

    Implemented some functions in ComplexFunction

--- Ex1/ComplexFunction.java
+++ Ex1/ComplexFunction.java
@@ -5,38 +5,68 @@ public class ComplexFunction implements complex_function
 	private Operation _Op;
 	private function _Left;
 	private function _Right;
-	
+
 	public ComplexFunction()
 	{
-		//Empty ComplexFunction
+		this._Left= null;
+		this._Right = null;
+		this._Op = Operation.None;
 	}
 	public ComplexFunction(String Op, function left, function right)
 	{
-	    switch (Op)
-	    {
-	      case "Plus": this._Op = Operation.Plus;
-	      case "Times": this._Op = Operation.Times;
-	      case "Divid": this._Op = Operation.Divid;
-	      case "Max": this._Op = Operation.Max;
-	      case "Min": this._Op = Operation.Min; 
-	      case "Comp": this._Op = Operation.Comp;
-	      case "None": this._Op = Operation.None;
-	      case "Error": this._Op = Operation.Error;
-	      //default:    
-	    }
+		switch (Op)
+		{
+			case "Plus": this._Op = Operation.Plus;
+				break;
+			case "Times": this._Op = Operation.Times;
+			break;
+			case "Divid": this._Op = Operation.Divid;
+			break;
+			case "Max": this._Op = Operation.Max;
+			break;
+			case "Min": this._Op = Operation.Min; 
+			break;
+			case "Comp": this._Op = Operation.Comp;
+			break;
+			case "None": this._Op = Operation.None;
+			break;
+			case "Error": this._Op = Operation.Error;
+			//default:    
+		}
 		this._Left = left;
 		this._Right = right;
 	}
 
-	
-	
+	public ComplexFunction(function left) 
+	{
+		// TODO Auto-generated constructor stub
+		this._Left = left;
+		this._Op = Operation.None;
+	}
 	@Override
 	public double f(double x) 
 	{
 		// TODO Auto-generated method stub
-		this._Left.f(x);
-		this._Right.f(x);
-		return 0; //which to return?
+		switch(_Op.toString())
+		{
+			case "Plus":  return _Left.f(x)+_Right.f(x);
+			case "Times" :  return _Left.f(x)*_Right.f(x);
+			case "Divid" : return _Left.f(x)/_Right.f(x);
+			case "Max" : if(_Left.f(x)>_Right.f(x))
+							return _Left.f(x);
+						else
+							return _Right.f(x);
+			case "Min" : if(_Left.f(x)>_Right.f(x))
+							return _Right.f(x);
+						else
+							return _Left.f(x);
+			case "Comp" : 
+						if(_Right != null) 
+							return _Left.f(_Right.f(x));
+						else
+							return _Left.f(x);
+			default: throw new RuntimeException("The Operation is not vaild");
+		}
 	}
 
 	@Override
@@ -47,60 +77,119 @@ public class ComplexFunction implements complex_function
 	}
 
 	@Override
-	public function copy() {
+	public function copy() 
+	{
 		// TODO Auto-generated method stub
-		return null;
+		function ans = new ComplexFunction(_Op.toString(),_Left.copy(),_Right.copy());
+		return ans;
 	}
-
+	@Override
+	public boolean equals(Object other)
+	{
+		if(other instanceof ComplexFunction)
+		{
+			ComplexFunction f = (ComplexFunction)other;
+			boolean check=false;
+			if(this._Op.compareTo(f._Op) == 0) // check if the enums are equal
+			{
+				check = true;
+			}
+			return this._Left.equals(f._Left) && this._Right.equals(f._Right) && check ;
+		}
+		if(other instanceof function)
+		{
+			return this._Left.equals(other);
+		}
+		return false;
+	}
 	@Override
 	public void plus(function f1) 
 	{
-		// TODO Auto-generated method stub
-		if (f1 != null)
+		if(this._Right != null)
 		{
-			this._Right = f1;
-			this._Op = Operation.Plus;
+			function f = new ComplexFunction(_Op.toString(),this._Left.copy(),this._Right.copy());
+			this._Left = f;
 		}
+
+		this._Right = f1.copy();
+		this._Op = Operation.Plus;
 	}
 
 	@Override
-	public void mul(function f1) {
+	public void mul(function f1) 
+	{
+		if(this._Right != null)
+		{
+			function f = new ComplexFunction(_Op.toString(),this._Left,this._Right);
+			this._Left = f.copy();
+		}
+		this._Right = f1.copy();
+		this._Op = Operation.Times;
+
+	}
+
+	@Override
+	public void div(function f1) 
+	{
 		// TODO Auto-generated method stub
-		
+		if(this._Right != null)
+		{
+			function f = new ComplexFunction(_Op.toString(),this._Left,this._Right);
+			this._Left = f.copy();
+		}
+		this._Right = f1.copy();
+		this._Op = Operation.Divid;
 	}
 
 	@Override
-	public void div(function f1) {
+	public void max(function f1) 
+	{
 		// TODO Auto-generated method stub
-		
+		if(this._Right != null)
+		{
+			function f = new ComplexFunction(_Op.toString(),this._Left,this._Right);
+			this._Left = f.copy();
+		}
+		this._Right = f1.copy();
+		this._Op = Operation.Max;
 	}
 
 	@Override
-	public void max(function f1) {
+	public void min(function f1) 
+	{
 		// TODO Auto-generated method stub
-		
+		if(this._Right != null)
+		{
+			function f = new ComplexFunction(_Op.toString(),this._Left,this._Right);
+			this._Left = f.copy();
+		}
+		this._Right = f1.copy();
+		this._Op = Operation.Min;
 	}
 
 	@Override
-	public void min(function f1) {
+	public void comp(function f1) 
+	{
 		// TODO Auto-generated method stub
-		
+		if(this._Right != null)
+		{
+			function f = new ComplexFunction(_Op.toString(),this._Left,this._Right);
+			this._Left = f.copy();
+		}
+		this._Right = f1.copy();
+		this._Op = Operation.Comp;
 	}
 
 	@Override
-	public void comp(function f1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public function left() {
+	public function left() 
+	{
 		// TODO Auto-generated method stub
 		return this._Left; //should't be null
 	}
 
 	@Override
-	public function right() {
+	public function right() 
+	{
 		// TODO Auto-generated method stub
 		return this._Right; //can be null
 	}
